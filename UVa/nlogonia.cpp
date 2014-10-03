fe2e2ddad2e455b64b2a53458c241070c7fae8f2
Luis-Lamat <beto_alaska@hotmail.com>
2014-10-03 13:58:56 -0500

    Fixes nlogonia problem

--- UVa/nlogonia.cpp
+++ UVa/nlogonia.cpp
@@ -10,25 +10,31 @@ int main () {
   // initial user inputs
   int queries, centerX, centerY, X, Y;
   cin >> queries >> centerX >> centerY;
-  
-  // query while loop
-  while (queries--){
 
-    // house coordinates input
-    cin >> X >> Y;
-    
-    // resetting the string
-    string position = "";
+  do {
+    // query while loop
+    while (queries--){
 
-    // check house coordinates relative to the
-    // center point of the division
-    if (X == centerX || Y == centerY) // if house is in border
-      position = "divisa";
-    else {                            // else house is in other quadrant
-      position += (Y < centerY) ? "S" : "N";
-      position += (X < centerX) ? "O" : "E";
-    }
-    cout << position << endl;
-  }
+      // house coordinates input
+      cin >> X >> Y;
+
+      // resetting the string
+      string position = "";
+
+      // check house coordinates relative to the
+      // center point of the division
+      if (X == centerX || Y == centerY) // if house is in border
+        position = "divisa";
+      else {                            // else house is in other quadrant
+        position += (Y < centerY) ? "S" : "N";
+        position += (X < centerX) ? "O" : "E";
+      }
+      cout << position << endl;
+    }  
+    cin >> queries;
+    if (!queries) return 0;
+
+    cin  >> centerX >> centerY;
+  } while (queries);
   return 0;
 }
