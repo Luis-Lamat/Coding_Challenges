3ba91062c39a5a7785869cae2e17a22d9985cb58
Luis-Lamat <beto_alaska@hotmail.com>
2014-10-03 11:49:03 -0500

    Adds comments to Nlogonia problem

--- UVa/nlogonia.cpp
+++ UVa/nlogonia.cpp
@@ -1,19 +1,30 @@
+/*
+ * UVa problem: 11498 - Division of Nlogonia
+ */
 #include <iostream>
 #include <string>
 using namespace std; 
 
 int main () {
+
+  // initial user inputs
   int queries, centerX, centerY, X, Y;
   cin >> queries >> centerX >> centerY;
   
+  // query while loop
   while (queries--){
+
+    // house coordinates input
     cin >> X >> Y;
     
-    std::string position = "";
+    // resetting the string
+    string position = "";
 
-    if (X == centerX || Y == centerY)
+    // check house coordinates relative to the
+    // center point of the division
+    if (X == centerX || Y == centerY) // if house is in border
       position = "divisa";
-    else {
+    else {                            // else house is in other quadrant
       position += (Y < centerY) ? "S" : "N";
       position += (X < centerX) ? "O" : "E";
     }
